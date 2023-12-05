9cb8e4291c9ed88afc4bcef357b1d61c4357ecc6
gleguizamon <gonzaforce@gmail.com>
2023-12-05 00:35:01 -0300

    feat: clean example functions

--- src/functions/example-get/yup/validation.ts
+++ src/functions/example-get/yup/validation.ts
@@ -3,9 +3,7 @@ import { object, string } from 'yup';
 const querySchema = object({
   by: string().required('by is a required query param')
 }).nullable();
-
 const pathSchema = object({}).nullable();
-
 const bodySchema = object({}).nullable();
 
 export { querySchema, pathSchema, bodySchema };
--- src/functions/example-post/yup/validation.ts
+++ src/functions/example-post/yup/validation.ts
@@ -1,9 +1,7 @@
 import { object, string } from 'yup';
 
 const querySchema = object({}).nullable();
-
 const pathSchema = object({}).nullable();
-
 const bodySchema = object({
   message: string().required()
 }).required();
